7fbe4482806b03a9b04b0e4ebec6c42b491a7ffb
shalurai <shaluraics22@gmail.com>
2017-11-23 15:26:47 +0900

    dragging data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Draggable from 'react-draggable';
 
 
 
@@ -32,18 +33,39 @@ export default class App extends Component {
 
   render() {
     let showingData = this.state.todo.sort()
-    console.log(showingData);
+    console.log(showingData.length);
 
     return (
       <div className="App" align='center'>
         <h1>Jio Todo</h1>
+        <p>total: {showingData.length} projects </p>
         <input
          type = 'text'
          value = {this.state.newItem}
          onChange = { (event) => this.handleChange(event.target.value)}
          onKeyPress = { this.handleKeyPress}
         />
-       {showingData.map((todo, index) => <p key = { index }>{ todo}</p>) }
+         <div>
+         <table>
+         <thead>
+         <tr>
+         <td>Todolist</td>
+         <td>InProgress</td>
+         <td>Done</td>
+         </tr>
+         </thead>
+          <tbody>
+          {showingData.map((todo, index) =>
+            <Draggable>
+            <li key = { index }>
+             { todo }</li>
+            </Draggable>) }
+          </tbody>
+
+
+
+         </table>
+         </div>
       </div>
     );
   }
