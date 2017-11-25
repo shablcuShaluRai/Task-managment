4278962d1098b5514cb96b0747e4bfc250acc6d3
shalurai <shaluraics22@gmail.com>
2017-11-25 11:48:28 +0900

    updated styles

--- src/App.js
+++ src/App.js
@@ -39,18 +39,21 @@ export default class App extends Component {
 
   render() {
       return (
-      <div>
-      <div className="header container-fluid">
-        <div >
-              <h1> Jio Task Management </h1>
+        <div className="wholeApp">
+        <div className="header container-fluid">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1>Task Management </h1>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="count-bar total">
-        {this.state.projects.length} <br/> Projects
-      </div>
-      <div className="container">
-
-        <label>Add Project</label>
+        <div className="count-bar total">
+          {this.state.projects.length} <br/> Projects
+        </div>
+          <div className="container">
+            <div className="row">
+                <div className="col-xs-12">
+                  <label>Add Project</label>
         <input
          type = 'text'
          placeholder = 'Enter project'
@@ -59,32 +62,31 @@ export default class App extends Component {
          onKeyPress = { this.handleKeyPress}
         />
         </div>
-
-
-         <div className='container'>
-           <div onDragOver={this.dragOver}>
-               <div className='row'>
-                 <div className="todo block">
-                   <div className="block-header">
-                     <h4 data-type="todo">To do </h4>
-                     <div className="count-bar">{this.state.todo.length} <br/> PROJECTS</div>
-                   </div>
-                   <div className="block-body">
+      </div>
+  </div>
+  <div className="container todo-board">
+    <div className="row" onDragOver={this.dragOver.bind(this)}>
+        <div className="col-sm-4" data-type="todo">
+          <div className="todo block" data-type="todo">
+            <div className="block-header" data-type="todo">
+              <h4 data-type="todo">To do </h4>
+              <div className="count-bar" data-type="todo">{this.state.todo.length} <br/> PROJECTS</div>
+            </div>
+            <div className="block-body" data-type="todo">
                      <TodoItem
                         list={this.state.todo}
                         over={this.state.mainover}
                        id="todo" />
-                    </div>
-                 </div>
-               </div>
-               <div  className = 'col2'data-type="progress">
-               <div>
-                 <div className=" block" data-type="progress">
-                     <div className="block-header" data-type="progress">
-                       <h4 data-type="progress">  InProgress </h4>
-                       <div className="count-bar" data-type="progress">{this.state.progress.length} <br/> PROJECTS</div>
+                       </div>
                      </div>
-                     <div className="block-body" data-type="progress">
+                   </div>
+                   <div className="col-sm-4" data-type="progress">
+                     <div className="in-progress block" data-type="progress">
+                         <div className="block-header" data-type="progress">
+                           <h4 data-type="progress">  InProgress </h4>
+                           <div className="count-bar" data-type="progress">{this.state.progress.length} <br/> PROJECTS</div>
+                         </div>
+                         <div className="block-body" data-type="progress">
                       <TodoItem
                          list={this.state.progress}
                           over={this.state.mainover}
@@ -92,9 +94,8 @@ export default class App extends Component {
                      </div>
                  </div>
                </div>
-               </div>
-               <div  className='col3' data-type="done">
-                 <div className=" block" data-type="done">
+               <div className="col-sm-4" data-type="done">
+                 <div className="done block" data-type="done">
                      <div className="block-header" data-type="done">
                        <h4 data-type="done">  Done </h4>
                        <div className="count-bar" data-type="done">{this.state.done.length} <br/> PROJECTS</div>
